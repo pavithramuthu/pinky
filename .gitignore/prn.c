383ce0ddd0c85e2898eec1fdc2bcb7895abef444
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2017-08-05 16:20:36 +0530

    prn.c

---
+++ .gitignore/prn.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+return 0;
+}
