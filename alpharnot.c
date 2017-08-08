5e371fe7929006765a738f4b7e6ab3a7630998b5
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2017-08-08 16:57:46 +0530

    alpharnot.c

---
+++ alpharnot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int c;
+printf("enter the charcater");
+scanf("%c",&c);
+if(c>='a'&&c<='z')
+{
+printf("it is an alphabet");
+}
+else
+{
+printf("it is not an alphabet");
+}
+return 0;
+}
