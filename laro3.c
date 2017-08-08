d239321337c9d1434f954065c97c4d7ec5f11cbf
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2017-08-08 15:56:56 +0530

    laro3.c

---
+++ laro3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,a,b,c;
+printf("enter the number:");
+scanf("%d",&n);
+if(a>b&&a>c)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+return 0;
+}
