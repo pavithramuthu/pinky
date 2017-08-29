44a7f1d842ec3f0952b482cacb0f861538c668b7
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2017-08-29 15:43:02 +0530

    quit.c

---
+++ quit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#incude<windows.h>
+int j=0;
+int main()
+{
+printf("enter Q to quit !\n");
+while(1)
+{
+j++;
+printf("%d\n",j);
+Sleep(1000);
+system("cls");
+}
+return 0;
+}
