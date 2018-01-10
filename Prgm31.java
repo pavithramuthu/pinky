602cf2089ad5d08e94b58d2da0d24eb7d6e6add1
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-10 16:28:35 +0530

    Prgm31.java

--- Prgm31.java
+++ Prgm31.java
@@ -1,19 +1,23 @@
-import java.lang.*;
 import java.io.*;
 import java.math.*;
+import java.util.Scanner;
 public class Prgm31
 
 {
-	public static void main(String[] s)
+	public static void main(String s[])
 	{
-		int i,n=0,sum=0;
-		System.out.println("enter the string here:");
-		for(i=0;i<=n;i++)
+		String st="i love java programming";
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		st=sc.nextLine();
+		for(int i=0;i<st.length();i++)
 		{
-			System.out.println("the number of characters are:");
-			sum=sum+i;
-			i++;
+			count++;
+		}
+			System.out.println("count");
+			
+		
 		}
 	}
 	
-	}
+	
