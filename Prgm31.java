c2ba0d1afebe71287ea58012aa374c39807e52b5
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-10 16:07:41 +0530

    Prgm31.java

---
+++ Prgm31.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+public class Prgm31
+
+{
+	public static void main(String[] s)
+	{
+		int i,n=0,sum=0;
+		System.out.println("enter the string here:");
+		for(i=0;i<=n;i++)
+		{
+			System.out.println("the number of characters are:");
+			sum=sum+i;
+			i++;
+		}
+	}
+	
+	}
