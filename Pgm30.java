7da98b4ab5dd944214a67d39fdde49f500f167fc
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-17 16:01:38 +0530

    Pgm30.java

---
+++ Pgm30.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public Class Pgm30
+{
+public static void main(String s[])
+{
+Calendar now=Calendar.getInstance();
+System.out.println("current date:"+"current date"+"current month"+"current year");
+System.out.println("current minute:"+"current minurtes"+"current seconds");
+now=Calendar.getInstance();
+now=add(Calendar.Hour);
+Sytem.out.println("time before 1hours");
+}
+}
+
