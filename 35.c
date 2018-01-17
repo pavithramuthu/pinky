5b7b691fb4169e1db587b7d98abe85219c09ef38
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-17 16:35:18 +0530

    35.c

---
+++ 35.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[50];
+    int i,n=0,c=0;
+    scanf("%s",a);
+    n=strlen(a);
+     for(i=0;i<n;i++)
+{
+if(a[i]>='0' && a[i]<='9')
+c++;
+}
+printf("%d",c);
+	return 0;
+}
