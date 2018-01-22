edb90ba7aa7e6866674d8bae3a322ee514af83f2
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-22 16:43:40 +0530

    48.c

---
+++ 48.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("enter the numbers:");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	f=(a+b+c+d+e)/5;
+	printf("the number is %d",f);
+	return 0;
+}
