83cbc782416709060c509388b1524661a9803315
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-22 15:50:42 +0530

    Pgm30.java

--- Pgm30.java
+++ Pgm30.java
@@ -5,11 +5,11 @@ public Class Pgm30
 {
 public static void main(String s[])
 {
-Calendar now=Calendar.getInstance();
+Calendar c=new Calendar.getInstance();
 System.out.println("current date:"+"current date"+"current month"+"current year");
 System.out.println("current minute:"+"current minurtes"+"current seconds");
-now=Calendar.getInstance();
-now=add(Calendar.Hour);
+new=Calendar.getInstance();
+new=add(Calendar.Hour);
 Sytem.out.println("time before 1hours");
 }
 }
