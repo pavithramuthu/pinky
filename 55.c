e0e97d7f63fe0d0fa87d46018c72bc0fb669fe56
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-01-31 16:31:27 +0530

    55.c

---
+++ 55.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+printf("enter the numbers:");
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c%2==0)
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
