e510d85a78e984b6f083a6b418a330b2f7fab62a
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-07 16:47:35 +0530

    61.c

---
+++ 61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c[20];
+int n,i;
+printf("enter the character:");
+for(i=0;i<=10;i++)
+scanf("%c",&c[i]);
+printf("enter no.of letters to be printed:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	printf("%c",c[i]);
+}
+return 0;
+}
