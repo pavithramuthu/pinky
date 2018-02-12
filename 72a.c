4632be04216b2350236f87dcc8ded840eaf9613f
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-12 16:21:50 +0530

    72a.c

--- 72a.c
+++ 72a.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int main()
 {
-char c;
-printf("enter the STRING");
-scanf("%c",&c);
-if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U')
+char c[10];
+int i,count=0;
+printf("enter the STRING \n");
+for(i=0;i<10;i++)
+scanf("%s",&c[10]);
+if(c[i]=='a'||c[i]=='A'||c[i]=='e'||c[i]=='E'||c[i]=='i'||c[i]=='I'||c[i]=='o'||c[i]=='O'||c[i]=='u'||c[i]=='U')
 {
+count++;
+}
+if(count!=0)
+{
+	
 printf("it has vowel");
 }
 else
