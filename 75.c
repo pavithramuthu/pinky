b6b2e62579dbe85e590b1b75ddba51febae3ce2a
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-12 15:57:51 +0530

    75.c

---
+++ 75.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char str[10];
+int n;
+printf("enter the string:");
+scanf("%s",&str);
+n=strlen(str);
+if(n%2==0)
+{
+str[(n/2-1)]='*';
+}
+else
+{
+str[n/2]='*';
+}
+printf("%s",str);
+return 0;
+}
