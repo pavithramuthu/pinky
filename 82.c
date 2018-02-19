58224fa0758f1bd844e7f74f17e3bd57dd238108
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-19 16:54:46 +0530

    82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float x,y,z;
+printf("enter the length: \n");
+scanf("%f",&x);
+printf("enter the breadth \n");
+scanf("%f",&y);
+z=x*y;
+printf("area of farm %f",z);
+return 0;
+}
+
