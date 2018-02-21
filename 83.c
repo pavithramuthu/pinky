980eaf0d4f1cc2526db992c17e40f04a5ecba024
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-21 16:19:50 +0530

    83.c

---
+++ 83.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the a and b values:");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%d \n",c);
+printf("enter the a and b values:");
+scanf("%d%d",&a,&b);
+d=a%b;
+printf("%d \n",d);
+return 0;
+}
