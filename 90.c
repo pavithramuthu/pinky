dd9ef0704ac0ee97e187d2ec69428ac3e4cb791f
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-02-23 16:28:50 +0530

    90.c

---
+++ 90.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[10],i;
+printf("enter the string");
+scanf("%s",&a[i]);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]>='0' && a[i]<='9')
+{
+printf("%c",a[i]);
+}
+}
+return 0;
+}
