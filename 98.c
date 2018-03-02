7aa1ff1f0206416a8b6fd77f129432c667adbbbd
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-02 16:35:42 +0530

    98.c

---
+++ 98.c
@@ -0,0 +1,17 @@
+#include<Stdio.h>
+void main()
+{
+int n[10],a,i;
+printf("enter the numbers:");
+scanf("%d",&a);
+printf("enter the values:");
+for(i=1;i<=a;i++)
+scanf("%d",&n[i]);
+for(i=1;i<=a;i++)
+{
+if(n[i]!=i)
+{
+printf("%d",i);
+}
+}
+}
