2e7b94b5b62acee5ba4fde84d5bdfbafdfe95cbc
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-02 15:57:50 +0530

    72a.c

--- 72a.c
+++ 72a.c
@@ -4,15 +4,15 @@ int main()
 char c[10];
 int i,count=0;
 printf("enter the STRING \n");
-for(i=0;i<10;i++)
-scanf("%s",&c[10]);
+scanf("%c",&c[i]);
+for(i=0;i<5;i++)
 if(c[i]=='a'||c[i]=='A'||c[i]=='e'||c[i]=='E'||c[i]=='i'||c[i]=='I'||c[i]=='o'||c[i]=='O'||c[i]=='u'||c[i]=='U')
 {
 count++;
 }
 if(count!=0)
 {
-	
+
 printf("it has vowel");
 }
 else
@@ -21,3 +21,4 @@ printf("it has no vowel");
 }
 return 0;
 }
+
