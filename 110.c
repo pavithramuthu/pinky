180e912efa5a416d5fcaaa12c6370c463f325b48
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-05 16:13:55 +0530

    110.c

---
+++ 110.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n,sum=0;;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum of numbers %d",sum);
+return 0;
+}
