f73f370a8b481551cf3b32c977100686f8680e7c
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-05 15:37:54 +0530

    94.c

---
+++ 94.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,n[10],i;
+printf("enter the values:");
+scanf("%d",&a);
+printf("enter the digit:");
+scanf("%d",&b);
+printf("enter the N digit:");
+for(i=0;i<a;i++)
+{
+scanf("%d",&n[i]);
+}
+printf("the value is %d",n[b-1]);
+return 0;
+}
