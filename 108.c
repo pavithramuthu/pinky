aba3b8d6bedde8518716adaf64cbe0ef173a0cb4
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-07 16:23:50 +0530

    108.c

--- 108.c
+++ 108.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int a,d,n,p;
+int a,d,n;
 printf("enter the first term:");
 scanf("%d",&a);
 printf("enter difference:");
 scanf("%d",&d);
 printf("enter no of terms:");
 scanf("%d",&n);
-p=(2*a)+(n-1)*d;
-printf("%d",p);
+int sum=0,i;
+for(i=0;i<n;i++)
+{
+sum=sum+(a+i*d);
+}
+printf("%d",sum);
 return 0;
 }
