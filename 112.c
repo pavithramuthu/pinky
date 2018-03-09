1903d4080830b48aed4f80c2a27398e531a7017d
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-09 16:40:18 +0530

    112.c

---
+++ 112.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i;
+int a[100],c=0;
+printf("enter n and k values:");
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+c++;
+}
+if(c!=0)
+printf("yes");
+else
+printf("no");
+return 0;
+}
