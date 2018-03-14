750bf3418c5a2d6c3859a06d90b74e1fffc40558
pavithraparameswaran <30748570+pavithraparameswaran@users.noreply.github.com>
2018-03-14 16:05:13 +0530

    211p.c

---
+++ 211p.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int b,h,a;
+printf("enter the base:\n");
+scanf("%d",&b);
+printf("enter the height:\n");
+scanf("%d",&h);
+a=0.5*b*h;
+printf("area of triangle is \n%d",a);
+return 0;
+}
